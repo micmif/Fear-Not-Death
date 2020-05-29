3939432814b6eb982ec53303ddb78bdefb7e772f
Michela <43954841+micmif@users.noreply.github.com>
2020-05-29 23:23:44 +0200

    Movement + Scene Transitions
    First Build

---
+++ Fear_Not_Death/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+   private float timeBtwAttack;
+   public float startTimeBtwAttack;
+
+   public Transform attackPos;
+   public LayerMask whatIsEnemies;
+   public float attackRange;
+
+//    void Update(){
+//        if(timeBtwAttack <= 0)
+//        {
+//            if(Input.GetKey(KeyCode.Space))
+//            {
+//                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+//                for (int i = 0; i < enemiesToDamage.Length; i++)
+//                {   
+//                  enemiesToDamage[i].GetComponent<Enemy>().health -= damage;  
+//                }
+//            }
+//            timeBtwAttack = startTimeBtwAttack;
+//        } else {
+//            timeBtwAttack -= Time.deltaTime;
+//        }
+//    }
+}
---
+++ Fear_Not_Death/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public string sceneToLoad;
+    public void OnCollisionEnter2D (Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
